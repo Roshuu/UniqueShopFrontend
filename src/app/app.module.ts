2aca949cd68e8ab596fcf37c1ac400f626803e8e
Roshuu <jaqub3k2001@gmail.com>
2023-12-24 01:54:37 +0100

    fullpage layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
+import { FullpageComponent } from './layouts/fullpage/fullpage.component';
+import { FullpageModule } from './layouts/fullpage/fullpage.module';
+import { LoginComponent } from './modules/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DefaultModule,
+    FullpageModule
+    
   ],
   providers: [
     provideClientHydration()
